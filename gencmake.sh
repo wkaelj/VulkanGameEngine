065883c3c46e7799f9127790015c43df30327f12
MisterTeddy <85797195+MisterTeddyDev@users.noreply.github.com>
2022-02-16 07:01:15 -0700

    Update gencmake.sh

--- gencmake.sh
+++ gencmake.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 #create build folder
+<<<<<<< Updated upstream
 mkdir -p build
 cp -r Config build/
+=======
+mkdir -p build/Config/shaders
+#copy config files into build folder
+cp Config/*.txt build/Config/*.txt
+>>>>>>> Stashed changes
 
 ./compile_shaders.sh
 #run cmake
